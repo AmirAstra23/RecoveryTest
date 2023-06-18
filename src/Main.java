93a7c66db6fb09f755c447a291bfc22f47e7ae99
AmirMurmansk Astra <Amirastra78@gmail.com>
2023-06-18 20:35:03 +0300

    magic file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // создаем объект File в качестве каталога
+
+        String text = "Hello world!";
+// откроем байтовый поток записи в файл
+        try (FileOutputStream fos = new FileOutputStream("notes.txt")) {
+// перевод строки в массив байтов
+            byte[] bytes = text.getBytes();
+// запись байтов в файл
+            fos.write(bytes, 0, bytes.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        // откроем байтовый поток для чтения файла
+        try (FileInputStream fin = new FileInputStream("notes.txt")) {
+            int i;
+// считываем файл пока есть доступные байты
+            while ((i = fin.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
